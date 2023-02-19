53f01e872cafc7adc3b2073fe344771bae88aec6
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2023-02-19 02:08:54 +0200

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using BepInEx;
+using BepInEx.Configuration;
+
+namespace TimeLimit
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public ConfigEntry<int> minutes;
+        public ConfigEntry<int> seconds;
+
+        private float timeRemaining;
+
+        private bool gameStarted = false;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            minutes = Config.Bind("Time Limit", "Minutes", 1, "The amount of minutes in the time limit.");
+            seconds = Config.Bind("Time Limit", "Seconds", 30, "The amount of seconds in the time limit.");
+
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        private void Update()
+        {
+            if(timeRemaining > 0 && gameStarted)
+            {
+                timeRemaining -= Time.deltaTime;
+
+                Logger.LogInfo(timeRemaining);
+            }
+            else if (timeRemaining <= 0 && gameStarted)
+            {
+                RestartGame();
+
+                gameStarted = false;
+
+                Logger.LogInfo("Time ran out.");
+            }
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if(scene.name == "Mian")
+            {
+                timeRemaining = minutes.Value * 60 + seconds.Value;
+
+                gameStarted = true;
+
+                Logger.LogInfo("Timer Started");
+            }
+            else
+            {
+                gameStarted = false;
+            }
+        }
+
+        private void RestartGame()
+        {
+            PlayerPrefs.DeleteKey("NumSaves");
+            PlayerPrefs.DeleteKey("SaveGame0");
+            PlayerPrefs.DeleteKey("SaveGame1");
+            PlayerPrefs.Save();
+
+            SceneManager.LoadScene("Mian");
+        }
+
+        private void OnGUI()
+        {
+            if(SceneManager.GetActiveScene().name == "Loader")
+            {
+                if(GUI.Button(new Rect(10, 10, 200, 100), "Test"))
+                {
+                    Logger.LogInfo("Button Pressed");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Time Limit Plugin for Getting Over It with Bennett Foddy
+
+The Time Limit plugin is a BepInEx plugin for the game *Getting Over It with Bennett Foddy*. It allows you to set a time limit for your speedruns, and will automatically restart your run if you exceed that time limit.
+
+## Installation
+
+1. Download the latest release of BepInEx from [its GitHub page](https://github.com/BepInEx/BepInEx/releases).
+2. Extract the context of the BepInEx zip file to the Getting Over It root directory.
+3. Run the game once to generate the necessary files.
+4. Download the latest release of the Time Limit plugin from [the releases page](https://github.com/mrboogybam/time-limit/releases).
+5. Extract the contents of the zip file to the `Getting Over It/BepInEx/plugins` directory.
+6. Launch the game and the plugin will be active.
+
+## Usage
+
+The Time Limit plugin will automatically check the time of your speedrun and restart it if the time limit is exceeded. To set the time limit, follow these steps:
+
+TBD
+
+## Support
+
+If you encounter any issues with the plugin, please [submit an issue](https://github.com/mrboogybam/time-limit/issues/new).
+
+## License
+
+The Time Limit plugin is licensed under the [MIT License](https://github.com/mrboogybam/time-limit/blob/main/LICENSE).
